e3126548389616d9bd8b8f7968156ccf1995d12d
14218 <14218@FlowKuang.wusm-wifi.wucon.wustl.edu>
2023-10-04 14:13:32 -0500

    LiZhangStudio4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,22 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape=in.next();
+		int R=in.nextInt();
+		int G=in.nextInt();
+		int B=in.nextInt();
+		Color hahaha = new Color(R, G, B);
+		Boolean tf=in.nextBoolean();
+		double x1=in.nextDouble();
+		double y1=in.nextDouble();
+		double x2=in.nextDouble();
+		double y2=in.nextDouble();
+		double x3=in.nextDouble();
+		double y3=in.nextDouble();
+		StdDraw.setPenColor(hahaha); //or use a custom color here!
+        double[] xVertices = { x1, x2, x3 };
+        double[] yVertices = { y1, y2, y3 };
+	     StdDraw.filledPolygon(xVertices, yVertices);
+		//StdDraw.filledSquare(0.5, 0.5, 0.2);
 	}
 }
